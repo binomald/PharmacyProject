1499f40cdd95f0e914659ab894fdf4b32d11ab5f
Asus TUF <Asus TUF@DESKTOP-P1VJHL0>
2025-05-10 21:04:14 +0500

    #pidoras
    Обновление интерфейса и удаление MyUserControl
    
    - Удален заголовочный файл `MyUserControl.h` из `MyForm.h`.
    - Удален файл `MyUserControl.cpp` из проекта и связанных файлов.
    - Изменены размеры, расположение и свойства `panel1` и кнопок.
    - Добавлен обработчик события `button1_Click` в `MyForm.h`.
    - Обновлены параметры `MyForm` (`AutoScaleDimensions`, `ClientSize`, шрифт).
    - В `MyForm.resx` добавлено метаданное `$this.TrayHeight`.
    
    Добавьте файлы проекта.

---
+++ MyForm.h
@@ -0,0 +1,155 @@
+#pragma once
+
+
+namespace PharmacyProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::ColorDialog^ colorDialog1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+
+
+
+
+
+
+
+
+
+
+	protected:
+
+
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->panel1->Controls->Add(this->button3);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Location = System::Drawing::Point(2, 0);
+			this->panel1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(336, 932);
+			this->panel1->TabIndex = 0;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button3->FlatAppearance->BorderColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Location = System::Drawing::Point(40, 432);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(296, 80);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button2->FlatAppearance->BorderColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Location = System::Drawing::Point(40, 295);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(296, 80);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button1->FlatAppearance->BorderColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point(40, 161);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(296, 77);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 21);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1350, 676);
+			this->Controls->Add(this->panel1);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe Print", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->panel1->ResumeLayout(false);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
